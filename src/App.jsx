6dbd28c7f421d387241d6486266b64ecd6ab44ef
Hima Kishore <himakishore2801@gmail.com>
2025-12-24 19:28:09 +0530

    Added Orders and Tracking pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage.jsx'
-import { CheckoutPage } from './pages/CheckoutPage.jsx'
+import { HomePage } from './pages/HomePage.jsx';
+import { CheckoutPage } from './pages/CheckoutPage.jsx';
+import { Orders } from './pages/Orders.jsx';
+import { Tracking } from './pages/Tracking.jsx';
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="Orders" element={<Orders />} />
+      <Route path="Tracking" element={<Tracking />} />
     </Routes>
   );
 }
