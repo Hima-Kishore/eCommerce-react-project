67c7c01855a3e86fc5a45018964fd1249f44b6ae
Hima Kishore <himakishore2801@gmail.com>
2025-12-25 22:43:59 +0530

    Implemented 'axios' and used backend data to fetch product details in the homepage

--- eCommerce-frontend/src/pages/HomePage.jsx
+++ eCommerce-frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from "../components/Header.jsx";
-import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
 
 export function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response)=>{
+        setProducts(response.data);
+      });
+  }, []);
+  
+
   return (
     <>
       <title>Ecommerce Project</title>
